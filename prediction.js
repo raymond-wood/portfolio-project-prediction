283ef1905772ad4d3078324bb562c181aefe5a63
raymond-wood <87679808+raymond-wood@users.noreply.github.com>
2022-04-29 12:12:55 -0700

    Initial Commit

--- prediction.js
+++ prediction.js
@@ -2,11 +2,26 @@
 prediction message in the console on 
 each run - Raymond Wood - 29-Apr-2022 */
 
-const predictionArray = ['You will meet a tall dark stranger.', 'You will come into financial success.', 'The sun will shine on you today.'];
-let i = 0;
+// Add/remove/update prediction values here in this array
+const predictionArray = [
+    'you will meet a tall dark stranger.',
+    'you will come into financial success.',
+    'the sun will shine on you today.',
+    'the stock markets will crash.',
+    'choas will ensue.',
+    'you will accompish great things.',
+    'you will reunite with an old friend.',
+    'you will meet the love of your life.',
+    'it will rain today.',
+    'tradgedy will occur.'];
+
+//Choose and return a prediction at random
+function makePrediction(arr) {
+    const index = Math.floor(Math.random() * arr.length);
+
+    return arr[index];
+}
+
+console.log(`I predict that ${makePrediction(predictionArray)}`);
 
 
-
-
-
-console.log(`I predict that: ${predictionArray[i]}`);
