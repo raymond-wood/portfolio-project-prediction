696066d33a915d12b1914aba09c3d8633d90d851
raymond-wood <87679808+raymond-wood@users.noreply.github.com>
2022-04-29 09:41:01 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#Prediction Generator
+
+###It will tell your future!
+
+by Raymond Wood, 29 April 2022 for Codecademy Full Stack Engineer Course
+
+ 
---
+++ prediction.js
@@ -0,0 +1,12 @@
+/* Prediction.js will produce a random
+prediction message in the console on 
+each run - Raymond Wood - 29-Apr-2022 */
+
+const predictionArray = ['You will meet a tall dark stranger.', 'You will come into financial success.', 'The sun will shine on you today.'];
+let i = 0;
+
+
+
+
+
+console.log(`I predict that: ${predictionArray[i]}`);
